9410c37e9c692c154f7cde0be910a81812ebd8e2
Luz Colunga <luz.colunga@udem.edu>
2023-08-10 22:02:31 -0600

    main

---
+++ Program.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.PixelFormats;
+using NReco.VideoConverter;
+
+//Notes: This program can be used with any video, it jsut dont play any sound
+
+namespace VideoPlay
+{
+    class Program 
+    {
+        static void Main(string[]args){
+
+            string FileName = "bad_apple.mp4";
+            Console.WriteLine("Video playing...";)
+
+        }
+        
+
+    }
+}
+
